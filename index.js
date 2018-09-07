de38f5d259455d908792f79ea7d69fa9e5da548c
Omar A <oazzat19@gmail.com>
2018-09-07 13:06:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  var tot = document.querySelectorAll("ul.ranked-list li")
+  
+  for(var i=0; i<tot.length; i++){
+    tot[i].innerHTML = parseInt(tot[i].innerHTML)+n
+  }
+}
+
+function deepestChild() {
+  var grandNode = document.querySelector('#grand-node div div div div');
+  //var deepestNode = grandNode.children[0];
+  //for (var i = 0; !deepestNode.children[i]; i) {
+  //  deepestNode = deepestNode.children[0]
+  //}
+  return grandNode;
+}
